46d8a9faf2318623638e2875c4169501957918ea
Timh B <timh@qvzhosting.com>
2013-09-10 12:07:34 +0200

    Encaps the requests-get in a try-except clause to handle errors. A failed request will just return 0.0.

--- marketvalue.py
+++ marketvalue.py
@@ -5,16 +5,25 @@
 import requests
 
 def get_mtgox_price():
-    btcusd = requests.get('https://mtgox.com/api/1/BTCUSD/ticker').json()
-    return btcusd['return']['last']['value']
+    try:
+        btcusd = requests.get('https://mtgox.com/api/1/BTCUSD/ticker').json()
+        return btcusd['return']['last']['value']
+    except:
+        return 0.0
 
 def get_btce_price():
-    btcusd = requests.get('https://btc-e.com/api/2/btc_usd/ticker').json()
-    return btcusd['ticker']['last']
+    try:
+        btcusd = requests.get('https://btc-e.com/api/2/btc_usd/ticker').json()
+        return btcusd['ticker']['last']
+    except:
+        return 0.0
 
 def get_bitstamp_price():
-    btcusd = requests.get('https://www.bitstamp.net/api/ticker/').json()
-    return btcusd['last']
+    try:
+        btcusd = requests.get('https://www.bitstamp.net/api/ticker/').json()
+        return btcusd['last']
+    except:
+        return 0.0
 
 if __name__ == '__main__':
     prices = {'btce': get_btce_price(), 'mtgox': get_mtgox_price(), 'bitstamp': get_bitstamp_price()}
